b5355862b216e8f4a95f6daeb70aee2c0234729d
Ayush Goel <ayush.g@directi.com>
2013-07-28 02:04:29 +0530

    Try to parse the number without country code first

--- parse_nums.py
+++ parse_nums.py
@@ -7,10 +7,27 @@ import sys
 def get_phone_fields(fields):
   return [fields.index(field) for field in fields if ((field.find('Phone') != -1) and (field.find('Value') != -1))]
 
+def should_process_phone(phone):
+  return phone.find(':::') == -1
+
+def phone_object_for_phone(phone):
+  try:
+    return pn.parse(phone)
+  except pn.phonenumberutil.NumberParseException:
+    try:
+      return pn.parse(phone, 'IN')
+    except pn.phonenumberutil.NumberParseException:
+      print("Couldn't parse number", phone)
+      return None
+
 def process(row, phone_fields):
   phone_nums = [row[i] for i in phone_fields if row[i]]
   for phone in phone_nums:
-    None  
+    ph = phone_object_for_phone(phone)
+    if ph:
+      print (phone, " -> ", pn.format_number(ph, pn.PhoneNumberFormat.E164))
+    else:
+      print ("Phone number couldn't be parsed")
 
 
 def start():
