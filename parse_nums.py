96aecd19f9c6a0477f262a1826f14d61e299e131
Ayush Goel <ayush.g@directi.com>
2013-08-18 23:00:03 +0530

    Give i/o files and country code as arg to functions

--- parse_nums.py
+++ parse_nums.py
@@ -15,25 +15,25 @@ def get_phone_fields(fields):
 def should_process_phone(phone):
   return phone.find(MULTIPLE_PHONE_SEPARATOR) == -1
 
-def match_phone_numbers(phone_str):
-  return [match.number for match in pn.PhoneNumberMatcher(phone_str, 'IN')]
+def match_phone_numbers(phone_str, country_code):
+  return [match.number for match in pn.PhoneNumberMatcher(phone_str, country_code)]
 
-def phone_object_for_phone(phone):
+def phone_object_for_phone(phone, country_code):
   try:
     return pn.parse(phone)
   except pn.phonenumberutil.NumberParseException:
     try:
-      return pn.parse(phone, 'IN')
+      return pn.parse(phone, country_code)
     except pn.phonenumberutil.NumberParseException:
       print("Couldn't parse number", phone)
-      matched_phone_numbers = match_phone_numbers(phone)
+      matched_phone_numbers = match_phone_numbers(phone, country_code)
       if matched_phone_numbers:
         return matched_phone_numbers
       return None
 
-def boil_phone_numbers(phone_nums):
+def boil_phone_numbers(phone_nums, country_code):
   for phone in phone_nums:
-    ph = phone_object_for_phone(phone)
+    ph = phone_object_for_phone(phone, country_code)
     if ph and isinstance(ph, list):
       phone_separator = MULTIPLE_PHONE_SEPARATOR.center(len(MULTIPLE_PHONE_SEPARATOR) + 2, ' ') 
       return phone_separator.join([pn.format_number(i, pn.PhoneNumberFormat.E164) for i in ph])
@@ -43,17 +43,17 @@ def boil_phone_numbers(phone_nums):
       print ("Phone number couldn't be parsed", phone_nums)
       return None
 
-def process(row, phone_fields):
+def process(row, phone_fields, country_code):
   newrow = list(row)
   for i in phone_fields:
     if row[i]:
-      newrow[i] = boil_phone_numbers([row[i]])
+      newrow[i] = boil_phone_numbers([row[i]], country_code)
       print(row[i], newrow[i])
   return newrow
 
-def start():
-  with open('google.csv', encoding = FILE_ENCODING) as rf:
-    with open('formatted_google.csv', 'w', encoding = FILE_ENCODING) as wf:
+def start(read_file_name, write_file_name, country_code):
+  with open(read_file_name, encoding = FILE_ENCODING) as rf:
+    with open(write_file_name, 'w', encoding = FILE_ENCODING) as wf:
       formatted_data = csv.reader(rf)
       fields = next(formatted_data)
       output_csv = csv.writer(wf, dialect = formatted_data.dialect)
@@ -62,11 +62,16 @@ def start():
       print(phone_fields)
       output_csv.writerow(fields)
       for row in formatted_data:
-        output_csv.writerow(process(row, phone_fields))
+        output_csv.writerow(process(row, phone_fields, country_code))
 
 def main():
-  if sys.version_info.major >= 3:
-    start()
+  if sys.version_info.major < 3:
+    return
+
+  read_file = 'google.csv'
+  write_file = 'formatted_google.csv'
+  country_code = 'IN'
+  start(read_file, write_file, country_code)
 
 main()
 
